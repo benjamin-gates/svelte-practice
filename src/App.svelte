8a90e5bd2b746e34eb59b285bf0a445081d4d3a3
Ben Gates <benjamin_gates@outlook.com>
2021-09-20 19:15:15 -0600

    practice with nested components, props, and reactiveness

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Sample from "./Sample.svelte";
+	let name = "people";
+</script>
+
+<h1>Hello {name}!</h1>
+<Sample count={1}/>
---
+++ src/Sample.svelte
@@ -0,0 +1,23 @@
+<script>
+    let message = "today, we will be learning Svelte!";
+    export let count;
+    function handleCount(){
+        count += 1;
+    }
+</script>
+<div>
+    <h1>Here is your Message!</h1>
+<p>{message}</p>
+<button on:click={handleCount}>Click Me! {count}</button>
+</div>
+
+<style>
+    p {
+        font-weight: bold;
+        font-style: italic;
+    }
+    button {
+        border-radius: 50%;
+        border-color: green;
+    }
+</style>
